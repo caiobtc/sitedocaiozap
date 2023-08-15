fcb93c501ae47e37e7c6446e63751329c19a61f8
Caio Vieira <50757916+caiobtc@users.noreply.github.com>
2023-08-15 10:20:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,20 @@ app = Flask(__name__)
 Socketio = SocketIO(app, cors_allowed_origins="*")
 
 # funcionalidade de enviar mesagem
+
+
 @Socketio.on("message")
 def gerenciar_menssagem(mensagem):
     send(mensagem, broadcast=True)
 
 # criar a nossa primeira pagina = primeira rota
+
+
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
+
 # roda o nosso aplicativo
-Socketio.run(app, host="192.168.0.35")
+if __name__ == "__main__":
+    Socketio.run(app, host="192.168.0.35")
